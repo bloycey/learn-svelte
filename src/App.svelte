9a7cc591e280e9538dbbac548aa42d72e2ba1dae
Andrew Bloyce <bloyce1990@gmail.com>
2020-01-27 14:17:42 +1000

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	let name = "Andrew";
+	let age = 30;
+
+	$: uppercaseName = name.toUpperCase();
+
+	const incrementAge = () => {
+		age += 1;
+	};
+
+	const changeName = () => {
+		name = "A different name"
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {uppercaseName}! My age is {age}</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
