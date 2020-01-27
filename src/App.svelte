85a29d6bac74631a199f226ae18a8fff364d5a52
Andrew Bloyce <bloyce1990@gmail.com>
2020-01-27 14:52:58 +1000

    Unidirectional flow

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@
 	let age = 30;
 
 	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === "A different name"){
+		age = 105;
+	}
 
 	const incrementAge = () => {
 		age += 1;
@@ -10,6 +14,10 @@
 
 	const changeName = () => {
 		name = "A different name"
+	}
+
+	const nameInput = event => {
+		name = event.target.value;
 	}
 </script>
 
@@ -21,4 +29,7 @@
 
 <h1>Hello {uppercaseName}! My age is {age}</h1>
 <button on:click={incrementAge}>Change Age</button>
-<button on:click={changeName}>Change Name</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<!-- <input type="text" value={name} on:input={nameInput}> -->
+<input type="text" bind:value={name}>
+
